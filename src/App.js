f45d78cb70d06099930578f530e3f01a27288237
Rabeet Abdul Rasheed <rabeetsati@gmail.com>
2023-07-31 01:09:52 +0500

    checkout added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter'
-import ProductList from './features/products/ProductList';
 import Home from './pages/Home';
 import LoginUser from './components/User/LoginUser';
 import SignupUser from './components/User/SignupUser';
 import { createBrowserRouter, RouterProvider, Route, Link } from 'react-router-dom'
 import CartPage from './pages/cart/CartPage';
+import Checkout from './pages/Checkout/Checkout';
 
 const router = createBrowserRouter([
   {
@@ -24,12 +23,15 @@ const router = createBrowserRouter([
     path: '/cart',
     element: <CartPage />,
   },
+  {
+    path: '/checkout',
+    element: <Checkout />,
+  },
 ])
 
 function App() {
   return (
     <div className="App">
-      
       <RouterProvider router={router} />
     </div>
   );
